0740104345d1ec738b654ffc2559c1d41cdb79a2
ADAIR7676 <adair2elias@gmail.com>
2026-01-28 00:27:22 -0600

    Intro-Flask

--- app.py
+++ app.py
@@ -1,60 +1,82 @@
 from flask import Flask, render_template, request
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
 
+import forms
 
 app=Flask(__name__)
+app.secret_key='clave secreta'
+
+csrf=CSRFProtect()
 
 
 @app.route("/")
 def index():
-    titulo = "Flask IDGS805"
-    lista = ["Juan", "Pedro", "Mario"]
+    titulo="Flask IDGS805"
+    lista=["Juan","Pedro","Mario"]
     return render_template("index.html", titulo=titulo, lista=lista)
 
-@app.route("/alumnos")
+@app.route("/alumnos",methods=['GET','POST'])
 def alumnos():
-    return render_template("alumnos.html")
+    mat=0
+    nom=""
+    apa=""
+    ama=""
+    email=""
+    alumnos_class=forms.UserForm(request.form)
+    if request.method=='POST' and alumnos_class.validate():
+        mat=alumnos_class.matricula.data
+        nom=alumnos_class.nombre.data
+        apa=alumnos_class.apellidoP.data
+        ama=alumnos_class.apellidoM.data
+        email=alumnos_class.correo.data
+        
+        mensaje='Bienvenido {}'.format(nom)
+        flash(mensaje)
 
-@app.route("/usuarios")
+    return render_template("alumnos.html",form=alumnos_class, mat=mat, nom=nom, apa=apa, ama=ama, email=email)
+
+@app.route("/usuarios", methods=['GET', 'POST']) 
 def usuarios():
     return render_template("usuarios.html")
 
 @app.route("/hola")
 def hola():
-    return "<h1>-----Hola Nuevo-----</h1>"
+    return "<h1>----Hola Nuevo----</h1>"
 
 @app.route("/user/<int:id>/<string:name>")
-def user(id,name):
+def user(id, name):
     return "ID: {} Nombre: {}".format(id,name)
 
 @app.route("/formulario")
 def formulario():
-    
     return '''
-        <form>
+    <form>
         <label for="name">Name:</label>
         <input type="text" id="name" name="name" required>
-        <br>
-        <label for="name">apaterno:</label>
+       
+        <label for="name">Paterno:</label>
         <input type="text" id="name" name="name" required>
     </form>
 '''
-@app.route("/operasBas", methods=['GET', 'POST'])
-def opera1():
-    if request.method == "POST":
-        n1 = request.form.get("n1")
-        n2 = request.form.get("n2")
+
+@app.route("/operasBas",methods=['GET','POST'])
+def operasBas():
+    res=0
+    if request.method=="POST":
+        n1=request.form.get("n1")
+        n2=request.form.get("n2")
         res = int(n1) + int(n2)
         
-        return render_template("operasBas.html", resultado=res)
-    
-    return render_template("operasBas.html")
+    return render_template("operasBas.html",res=res)
 
 @app.route("/resultado",methods=['GET','POST'])
 def resultado():
     n1=request.form.get("n1")
     n2=request.form.get("n2")
-    res=int(n1)+int(n2)
+    res = int(n1) + int(n2)
     return "La suma de {} + {} = {}".format(n1,n2,res)
 
-if __name__=='__main__':
+if __name__ =='__main__':
+    csrf.init_app(app)
     app.run(debug=True)
